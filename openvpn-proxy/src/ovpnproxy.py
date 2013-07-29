e82f1cc814748678e94743b19525b05e661a83dc
Vasu Chandrasekhara <vasu.chandrasekhara@gmail.com>
2013-07-29 17:40:03 +0200

    docu

--- openvpn-proxy/src/ovpnproxy.py
+++ openvpn-proxy/src/ovpnproxy.py
@@ -1,55 +1,12 @@
 """
 Usage 'ovpnproxy'
 
-OpenVPN Intercepting Multiplexer/Proxy 
+OpenVPN Intercepting Multiplexer/Proxy
+
 ============================================================
 Copyright (C) 2013 Vasu Chandrasekhara. All Rights Reserved.
 ============================================================
 
-Keep an ovpnproxy.cfg file with the following content:
-[Logging]
-#Optionally, write to a rotating logfile:
-# file = ovpnproxy.log
-# maxBytes = 102400
-# backupCount = 5
-loglevel = DEBUG
-#    The logging levels, in decreasing order of importance, are:
-# CRITICAL = 50
-# FATAL = CRITICAL
-# ERROR = 40
-# WARNING = 30
-# WARN = WARNING
-# INFO = 20
-# DEBUG = 10
-# DDEBUG = 5
-# DDDEBUG = 4
-
-[proxy]
-# listen for incoming requests here 
-ip = 0.0.0.0
-port = 9999
-
-[sinkvpn]
-# this is your default vpn sink
-ip = 172.19.136.144
-port = 443 
-
-# the follwoing sections represent the multiplexed VPN endpoints, 
-# based on the subject.CN in the client certificate
-# the subject.CN is specified as [section header]
-
-[client1]
-ip = 172.19.139.156
-port = 443 
-
-[client2]
-ip = 172.19.139.157
-port = 443 
-
-# the config file is dynamic
-============================================================
-Copyright (C) 2013 Vasu Chandrasekhara. All Rights Reserved.
-============================================================
 """
 
 import socket
